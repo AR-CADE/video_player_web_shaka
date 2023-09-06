d28d6fea65d08e29cb750bff6479f1a832386494
AR-CADE <armelcadetpetit@gmail.com>
2023-09-06 22:38:01 +0200

    Export video_player_web_shaka js api

--- lib/shaka.dart
+++ lib/shaka.dart
@@ -2,6 +2,7 @@
 library shaka;
 
 import 'package:video_player_web_shaka/video_player_web_shaka.dart';
+export 'package:video_player_web_shaka/shaka_js.dart';
 
 void configure(Map<String, Object> options) {
   VideoPlayerPluginShaka.options = options;
