8b1cf860f9a826203e7c6a4bfe8f59fee0341949
AR-CADE <arm-cade@proton.me>
2023-12-15 22:49:13 +0100

    Update shaka-player recommended version to 4.7.1

--- lib/no_script_tag_exception.dart
+++ lib/no_script_tag_exception.dart
@@ -1,5 +1,5 @@
 class NoScriptTagException implements Exception {
   @override
   String toString() =>
-      'Did you add   <script src="https://cdn.jsdelivr.net/npm/shaka-player@4.5.0/dist/shaka-player.compiled.min.js"  type="application/javascript"></script> in index.html? ';
+      'Did you add   <script src="https://cdn.jsdelivr.net/npm/shaka-player@4.7.1/dist/shaka-player.compiled.min.js"  type="application/javascript"></script> in index.html? ';
 }
